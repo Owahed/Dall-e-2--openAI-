067c2785e27a0e4d11e404318aaf4f8ab909c805
Owahed Hossain <owahedhossain@gmail.com>
2023-02-28 18:47:40 +0600

    loading

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Configuration, OpenAIApi } from "openai";
 function App() {
   const [prompt, setPrompt] = useState("");
   const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const configuration = new Configuration({
     apiKey: import.meta.env.VITE_OPEN_AI_API_KEY,
@@ -12,16 +13,18 @@ function App() {
   const openai = new OpenAIApi(configuration);
 
   const getImageGenerate = async () => {
+    setLoading(true);
     const res = await openai.createImage({
       prompt: prompt,
       n: 1,
       size: "1024x1024",
     });
     setResult(res.data.data[0].url);
+    setLoading(false);
   };
 
   return (
-    <div className="app-main">
+    <div className="app-main ">
       <h3>Generate an Image like DELL-E using Open AI (API)</h3>
       <input
         className="app-input"
@@ -30,6 +33,14 @@ function App() {
         onChange={(e) => setPrompt(e.target.value)}
       />
       <button onClick={getImageGenerate}>Generate Image</button>
+      {loading ? (
+        <div>
+          <span className="loading">Loading...</span>
+        </div>
+      ) : (
+        <></>
+      )}
+
       {result ? <img className="result-img" src={result} alt="" /> : <></>}
     </div>
   );
